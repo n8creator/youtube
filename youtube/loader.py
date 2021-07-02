8ccde0156eee326de57a717875656d6a2e77fa1d
n8creator <n8creator@pm.me>
2021-07-02 17:46:40 +0300

    loader.py refactored with DRY-principle

--- youtube/loader.py
+++ youtube/loader.py
@@ -3,9 +3,6 @@ from pytube.cli import on_progress
 from termcolor import colored
 
 
-DELIMITER = f'{"*"*80}'  # Delimiter used to format output text
-
-
 def list_all_streams(url):
     return YouTube(url=url).streams
 
@@ -32,59 +29,62 @@ def list_non_progressive_streams(url, type='video'):
         exit('Only "audio" or "video" are supported!')
 
 
-def download_progressive_video(url, filename='video'):
-    print(colored('Progressive video in .mp4 format downloading...'))
+def download_it(url: str, settings: dict):
+    print(colored(settings['intro_message']))
     try:
         YouTube(url=url, on_progress_callback=on_progress).streams.\
-            filter(progressive=True, file_extension='mp4').\
-            order_by('resolution').\
+            filter(**settings['params']).\
+            order_by(settings['order_by']).\
             desc().\
             first().\
-            download(filename=filename, skip_existing=False)
+            download(filename=settings['filename'], skip_existing=False)
     except Exception as error:
         print(f'Some error occured: {error}')
     else:
-        print(colored('\nVideo was successfully downloaded!\n', 'green'))
+        print(colored(settings['out_message'], 'green'))
 
 
-def download_mp4_video(url, filename='video'):
-    print(colored('Video in highest available resolution in .mp4 format '
-                  'downloading...'))
-    try:
-        YouTube(url=url, on_progress_callback=on_progress).streams.\
-            filter(progressive=False, only_video=True, file_extension='mp4').\
-            order_by('resolution').\
-            desc().\
-            first().\
-            download(filename=filename, skip_existing=False)
-    except Exception as error:
-        print(f'Some error occured: {error}')
-    else:
-        print(colored('\nVideo in .mp4 format was successfully downloaded!\n',
-                      'green'))
+PRESETS = {
+    'download_progressive_video': {
+        'intro_message': 'Progressive video in .mp4 format downloading...',
+        'params': {
+            'progressive': True,
+            'file_extension': 'mp4'
+        },
+        'order_by': 'resolution',
+        'filename': 'video_progressive',
+        'out_message': '\nVideo was sussessfully downloaded!\n'
+    },
+    'download_mp4_video': {
+        'intro_message': '.mp4 video in highest available resolution '
+                         'downloading...',
+        'params': {
+            'progressive': False,
+            'only_video': True,
+            'file_extension': 'mp4'
+        },
+        'order_by': 'resolution',
+        'filename': 'video',
+        'out_message': '\n.mp4 video was successfully downloaded!\n'
+    },
+    'download_mp4_audio': {
+        'intro_message': '.mp4 audio in highest available bitrate '
+                         'downloading...',
+        'params': {
+            'progressive': False,
+            'only_audio': True,
+            'file_extension': 'mp4'
+        },
+        'order_by': 'abr',
+        'filename': 'audio',
+        'out_message': '\n.mp4 audio was successfully downloaded!\n'
+    }
+}
 
-
-def download_mp4_audio(url, filename='audio'):
-    print(colored('Audio in highest available bitrate in .mp4 format '
-                  'downloading...'))
-    try:
-        YouTube(url=url, on_progress_callback=on_progress).streams.\
-            filter(progressive=False, only_audio=True, file_extension='mp4').\
-            order_by('abr').\
-            desc().\
-            first().\
-            download(filename=filename, skip_existing=False)
-    except Exception as error:
-        print(f'Some error occured: {error}')
-    else:
-        print(colored("\nAudio in .mp4 format was successfully downloaded!\n",
-                      'green'))
-
-
-url = 'https://www.youtube.com/watch?v=dAiuiU6VWNc'
+url = 'https://www.youtube.com/watch?v=nVliEYIBWFw'
 
 
 if __name__ == "__main__":
-    # download_mp4_video(url)
-    # download_mp4_audio(url)
-    download_progressive_video(url)
+    # download_it(url=url, settings=PRESETS['download_mp4_video'])
+    # download_it(url=url, settings=PRESETS['download_mp4_audio'])
+    download_it(url=url, settings=PRESETS['download_progressive_video'])
