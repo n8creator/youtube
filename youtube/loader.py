32c4b8df664ddded40698308efd3c22f0a9e735a
n8creator <n8creator@pm.me>
2021-07-03 09:16:51 +0300

    loader.py refactored with DRY-principle

--- youtube/loader.py
+++ youtube/loader.py
@@ -1,51 +1,12 @@
 from pytube import YouTube
 from pytube.cli import on_progress
 from termcolor import colored
+import re
+from pprint import pprint
 
 
-def list_all_streams(url):
-    return YouTube(url=url).streams
-
-
-def list_progressive_streams(url):
-    return YouTube(url=url).streams.\
-        filter(progressive=True, file_extension='mp4').\
-        order_by('resolution').\
-        desc()
-
-
-def list_non_progressive_streams(url, type='video'):
-    if type == 'audio':
-        return YouTube(url=url).streams.\
-            filter(progressive=False, only_audio=True).\
-            order_by('abr').\
-            desc()
-    elif type == 'video':
-        return YouTube(url=url).streams.\
-            filter(progressive=False, only_video=True).\
-            order_by('resolution').\
-            desc()
-    else:
-        exit('Only "audio" or "video" are supported!')
-
-
-def download_it(url: str, settings: dict):
-    print(colored(settings['intro_message']))
-    try:
-        YouTube(url=url, on_progress_callback=on_progress).streams.\
-            filter(**settings['params']).\
-            order_by(settings['order_by']).\
-            desc().\
-            first().\
-            download(filename=settings['filename'], skip_existing=False)
-    except Exception as error:
-        print(f'Some error occured: {error}')
-    else:
-        print(colored(settings['out_message'], 'green'))
-
-
-PRESETS = {
-    'download_progressive_video': {
+PROFILES = {
+    'progressive': {
         'intro_message': 'Progressive video in .mp4 format downloading...',
         'params': {
             'progressive': True,
@@ -55,7 +16,7 @@ PRESETS = {
         'filename': 'video_progressive',
         'out_message': '\nVideo was sussessfully downloaded!\n'
     },
-    'download_mp4_video': {
+    'video': {
         'intro_message': '.mp4 video in highest available resolution '
                          'downloading...',
         'params': {
@@ -67,7 +28,7 @@ PRESETS = {
         'filename': 'video',
         'out_message': '\n.mp4 video was successfully downloaded!\n'
     },
-    'download_mp4_audio': {
+    'audio': {
         'intro_message': '.mp4 audio in highest available bitrate '
                          'downloading...',
         'params': {
@@ -81,10 +42,61 @@ PRESETS = {
     }
 }
 
-url = 'https://www.youtube.com/watch?v=nVliEYIBWFw'
+
+def generate_filename(url):
+    yt = YouTube(url=url)
+    title = re.sub(r'[^\w\s-]', '', yt.title)  # remove all symbols
+    title = re.sub(r'\s+', ' ', title)  # remove recurring spaces
+    publish_date = yt.publish_date.strftime('%Y-%m-%d')
+    slug = yt.video_id
+
+    return f'{publish_date} - {title} [{slug}]'
+
+
+def list_all_streams(url):
+    return YouTube(url=url).streams
+
+
+def list_streams(url: str, settings: dict):
+    try:
+        yt = YouTube(url=url).streams.\
+            filter(**settings['params']).\
+            order_by(settings['order_by']).\
+            desc()
+    except Exception as err:
+        print(f'Some error occured: {err}')
+    else:
+        return list(yt)  # 'list' here needed to format output while printing
+
+
+def download(url: str, settings: dict):
+    print(colored(settings['intro_message']))
+    try:
+        YouTube(url=url, on_progress_callback=on_progress).streams.\
+            filter(**settings['params']).\
+            order_by(settings['order_by']).\
+            desc().\
+            first().\
+            download(filename=settings['filename'], skip_existing=False,
+                     timeout=10, max_retries=5)
+    except Exception as error:
+        print(f'Some error occured: {error}')
+    else:
+        print(colored(settings['out_message'], 'green'))
+
+
+url = 'https://www.youtube.com/watch?v=IMLwb8DIksk'
+url2 = 'https://www.youtube.com/watch?v=x4Xf4mmbecE'
+url3 = 'https://www.youtube.com/watch?v=RaqSk9S6WY0'
 
 
 if __name__ == "__main__":
-    # download_it(url=url, settings=PRESETS['download_mp4_video'])
-    # download_it(url=url, settings=PRESETS['download_mp4_audio'])
-    download_it(url=url, settings=PRESETS['download_progressive_video'])
+    print(generate_filename(url=url))
+
+    pprint(list_streams(url=url2, settings=PROFILES['progressive']))
+    pprint(list_streams(url=url2, settings=PROFILES['video']))
+    pprint(list_streams(url=url2, settings=PROFILES['audio']))
+
+    download(url=url2, settings=PROFILES['audio'])
+    download(url=url2, settings=PROFILES['video'])
+    download(url=url2, settings=PROFILES['progressive'])
